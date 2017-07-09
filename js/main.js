3881eb2289861a0d63b5f5f349ef78bb890bcd40
christinacthomas <christinacthomas@gmail.com>
2017-07-09 12:55:55 -0700

    Javascript algorithms

---
+++ js/main.js
@@ -0,0 +1,106 @@
+// FIZZBUZZ
+
+// for (i = 1; i <= 100; i++){
+
+// if (i % 3===0 && i % 5===0 )
+// 	console.log ("fizzbuzz")
+// else if (i % 3 == 0)
+// 	console.log("fizz")
+// else if (i % 5 == 0)
+// 	console.log("buzz")
+// else 
+// 	console.log(i)
+// }
+
+// LARGEST NUMBER
+
+// var randoNum = [3,56,234,42,1]
+// var largest = 0
+
+
+// for (var i = 0; i < randoNum.length; i++){
+//  	if (randoNum[i] > largest){
+//  		var largest=randoNum[i]
+//  	}
+// }
+
+// console.log(largest)
+
+
+// FIBONACCI SEQUENCE
+
+ // var a = 0
+ // var b = 1
+ // var c =[]
+
+ // while (c <=377){
+ // 	console.log(c)
+	// c = a;
+	// a = a + b;
+	// b = c;
+ //  }
+
+// Multiplication Table
+
+// for (var i = 1; i <=12; i++){
+// 	for (var j = 1; j <=12; j++){
+// 		console.log (i*j)
+// 	}
+// }
+
+
+// REVERSE STRING
+// var string = "hello there"
+// var reverseString = string.split(""). reverse().join("")
+// console.log (reverseString)
+
+
+// PALINDROME
+
+function noon(){
+var word = document.getElementByID('name').value;
+
+var palin = word.split(""). reverse().join("");
+
+if (word = palin){
+	console.log ("this is a palidrome")
+}
+
+else {
+	console.log ("this is not a palindrome")
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
